2f70e58b393393f99bef5eb6e6e62c6011ddd27f
Ricard <ricardbarnes@gmail.com>
2026-05-18 16:28:00 +0200

    made product deletion idempotent

--- src/main/java/es/mediamarkt/product/infrastructure/provider/products/h2/ForDeletingH2Products.java
+++ src/main/java/es/mediamarkt/product/infrastructure/provider/products/h2/ForDeletingH2Products.java
@@ -1,6 +1,5 @@
 package es.mediamarkt.product.infrastructure.provider.products.h2;
 
-import es.mediamarkt.product.domain.products.error.ProductNotFoundError;
 import es.mediamarkt.product.domain.products.model.ProductId;
 import es.mediamarkt.product.domain.products.port.ForDeletingProducts;
 import lombok.RequiredArgsConstructor;
@@ -14,9 +13,6 @@ public class ForDeletingH2Products implements ForDeletingProducts {
 
     @Override
     public void deleteById(ProductId value) {
-        if (!repository.existsById(value.value())) {
-            throw ProductNotFoundError.becauseOf(value);
-        }
         repository.deleteById(value.value());
     }
 
